e68c3cc2d01edbc9196d75137068e85f4835c06b
rurbano3d <rurbano3d@gmail.com>
2019-10-08 21:59:32 -0300

    chore:validation forms

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Email inválido')
+    .required('Email obrigatório'),
+  password: Yup.string().required('Senha obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -13,7 +20,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
